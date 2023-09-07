400c8c909416676c0a321ba6be3dd00a31aad36e
Natthaya-Polp <natthayacud56@gmail.com>
2023-09-08 01:18:07 +0700

    EDIT: Optimize

--- Assets/Scripts/Puchase/BuyButton.cs
+++ Assets/Scripts/Puchase/BuyButton.cs
@@ -7,27 +7,37 @@ public class BuyButton : MonoBehaviour
 {
     [SerializeField] private float startingCoins;
     [SerializeField] private float price;
+
     public float currentCoins{get; private set;}
+
     [SerializeField] private Text coinsText;
+
     public GameObject outOfMoney;
 
-    private void Start(){
+    private void Start()
+    {
         currentCoins = startingCoins;
         UpdateCoinsDisplay();
-
     }
-    public void LoseCoins(float _buy){
+
+    public void LoseCoins(float _buy)
+    {
         currentCoins = Mathf.Clamp(currentCoins - _buy, 0, startingCoins);
         UpdateCoinsDisplay();
 
-        if(currentCoins <= 0){
+        if(currentCoins <= 0)
+        {
             outOfMoney.SetActive(true);
         }
     }
-    public void OnButtonClick(){
+
+    public void OnButtonClick()
+    {
         LoseCoins(price);
     }
-    public void UpdateCoinsDisplay(){
+
+    public void UpdateCoinsDisplay()
+    {
         coinsText.text = " " + currentCoins.ToString();
     }
 }
